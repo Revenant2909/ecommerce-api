5200e6b9f49ba03f66bc1911a7861e00583755fa
Ritesh <riteshnahavre2909@gmail.com>
2023-05-06 15:09:14 +0530

    Update 1.1

--- routes/auth.js
+++ routes/auth.js
@@ -24,9 +24,11 @@ router.post("/register", async (req,res)=>{
 //LOGIN
 
 router.post("/login",async (req,res)=>{
+
     try {
      const user = await User.findOne({email:req.body.email});
-     !user && res.status(401).json("Wrong password or username!");
+
+     !user && res.status(401).json("Wrong password or Email!");
 
      const bytes  = CryptoJS.AES.decrypt(user.password,process.env.SECRET_KEY);
      const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
